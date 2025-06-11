deb335a76014ae18942e75ade5816a91ad9befe2
Justin Tapia <75755009+justjust1n1126@users.noreply.github.com>
2025-06-11 18:36:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Initial References
+
 const letterContainer = document.getElementById("letter-container");
 const optionsContainer = document.getElementById("options-container");
 const userInputSection = document.getElementById("user-input-section");
@@ -7,7 +7,7 @@ const newGameButton = document.getElementById("new-game-button");
 const canvas = document.getElementById("canvas");
 const resultText = document.getElementById("result-text");
 
-//Options values for buttons
+
 let options = {
   fruits: [
     "Apple", "Banana", "Orange", "Strawberry", "Grapes", "Watermelon", "Mango", "Pineapple", "Kiwi", "Avocado",
